471e862e69e77d8f0c87c6ef056b2c255fd19057
Minchan Kim <minchan.kim@gmail.com>
2011-10-31 17:06:51 -0700

    mm: compaction: make isolate_lru_page() filter-aware
    In async mode, compaction doesn't migrate dirty or writeback pages.  So,
    it's meaningless to pick the page and re-add it to lru list.
    
    Of course, when we isolate the page in compaction, the page might be dirty
    or writeback but when we try to migrate the page, the page would be not
    dirty, writeback.  So it could be migrated.  But it's very unlikely as
    isolate and migration cycle is much faster than writeout.
    
    So, this patch helps cpu overhead and prevent unnecessary LRU churning.
    
    Signed-off-by: Minchan Kim <minchan.kim@gmail.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Reviewed-by: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Acked-by: Rik van Riel <riel@redhat.com>
    Reviewed-by: Michal Hocko <mhocko@suse.cz>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	mm/compaction.c
    	mm/vmscan.c
    
    Conflicts:
    	mm/compaction.c

--- mm/compaction.c
+++ mm/compaction.c
@@ -326,7 +326,7 @@ isolate_migratepages_range(struct zone *zone, struct compact_control *cc,
 		}
 
 		if (!cc->sync)
-			mode |= ISOLATE_ASYNC_MIGRATE;
+			mode |= ISOLATE_CLEAN;
 
 		/* Try isolate the page */
 		if (__isolate_lru_page(page, mode, 0) != 0)
