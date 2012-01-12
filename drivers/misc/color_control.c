7f6c4485addf7efc58aa6bbf26be91721294fd61
Ezekeel <notezekeel@googlemail.com>
2012-01-12 20:49:31 +0100

    Added Color Control version 1.
    Conflicts:
    	drivers/misc/Makefile
    	drivers/misc/color_control.c
    	drivers/video/omap2/displays/panel-s6e8aa0.c

--- drivers/misc/color_control.c
+++ drivers/misc/color_control.c
@@ -1,6 +1,6 @@
 /* drivers/misc/color_control.c
  *
- * Copyright 2012  Ezekeel (based on supercurio's idea)
+ * Copyright 2012  Ezekeel
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -11,145 +11,26 @@
 #include <linux/device.h>
 #include <linux/miscdevice.h>
 
-#define COLORCONTROL_VERSION 4
+#define COLORCONTROL_VERSION 1
 
-extern void colorcontrol_update(bool multiplier_updated);
+extern void colorcontrol_update(int * v1_offsets);
 
-static bool safety_enabled = true;
+static int v1_offsets[] = {0, 0, 0};
 
-static int * v1_offset;
-
-static u32 * color_multiplier;
-
-static u32 original_multiplier[3];
-
-void colorcontrol_register_offset(int * offset)
+static ssize_t colorcontrol_v1offsets_read(struct device * dev, struct device_attribute * attr, char * buf)
 {
-    v1_offset = offset;
-
-    return;
-}
-EXPORT_SYMBOL(colorcontrol_register_offset);
-
-void colorcontrol_register_multiplier(u32 * multiplier)
-{
-    int i;
-
-    color_multiplier = multiplier;
-
-    for (i = 0; i < 3; i++)
-	original_multiplier[i] = color_multiplier[i];
-
-    return;
-}
-EXPORT_SYMBOL(colorcontrol_register_multiplier);
-
-static ssize_t colorcontrol_offset_read(struct device * dev, struct device_attribute * attr, char * buf)
-{
-    return sprintf(buf, "%i %i %i\n", v1_offset[0], v1_offset[1], v1_offset[2]);
+    return sprintf(buf, "%i %i %i\n", v1_offsets[0], v1_offsets[1], v1_offsets[2]);
 }
 
-static ssize_t colorcontrol_offset_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
-{
-    int i;
-    int new_offset[3];
-
-    if(sscanf(buf, "%i %i %i\n", &new_offset[0], &new_offset[1], &new_offset[2]) == 3) 
-	{
-	    for (i = 0; i < 3; i++)
-		v1_offset[i] = new_offset[i];
-
-	    pr_info("COLORCONTROL V1 offsets changed\n");
-
-	    colorcontrol_update(false);
-	} 
-    else 
-	{
-	    pr_info("%s: invalid input\n", __FUNCTION__);
-	}
-
-    return size;
-}
-
-static ssize_t colorcontrol_multiplier_read(struct device * dev, struct device_attribute * attr, char * buf)
-{
-    return sprintf(buf, "%u %u %u\n", color_multiplier[0], color_multiplier[1], color_multiplier[2]);
-}
-
-static ssize_t colorcontrol_multiplier_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
-{
-    int i;
-
-    u32 new_multiplier[3];
-
-    if(sscanf(buf, "%u %u %u\n", &new_multiplier[0], &new_multiplier[1], &new_multiplier[2]) == 3) 
-	{
-	    for (i = 0; i < 3; i++)
-		{
-		    if (safety_enabled)
-			color_multiplier[i] = min(new_multiplier[i], original_multiplier[i]);
-		    else
-			color_multiplier[i] = new_multiplier[i];
-		}
-
-	    pr_info("COLORCONTROL color multipliers changed\n");
-
-	    colorcontrol_update(true);
-	} 
-    else 
-	{
-	    pr_info("%s: invalid input\n", __FUNCTION__);
-	}
-
-    return size;
-}
-
-static ssize_t colorcontrol_safety_read(struct device * dev, struct device_attribute * attr, char * buf)
-{
-    return sprintf(buf, "%u\n", (safety_enabled ? 1 : 0));
-}
-
-static ssize_t colorcontrol_safety_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+static ssize_t colorcontrol_v1offsets_write(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
 {
     unsigned int data;
 
-    int i;
-
-    bool multipliers_modified = false;
-
-    if(sscanf(buf, "%u\n", &data) == 1) 
+    if(sscanf(buf, "%i %i %i\n", &v1_offsets[0], &v1_offsets[1], &v1_offsets[2]) == 3) 
 	{
-	    pr_devel("%s: %u \n", __FUNCTION__, data);
-	    
-	    if (data == 1) 
-		{
-		    pr_info("%s: COLORCONTROL safety enabled\n", __FUNCTION__);
+	    pr_info("COLORCONTROL V1 offsets changed\n");
 
-		    safety_enabled = true;
-
-		    for (i = 0; i < 3; i++)
-			{
-			    if (color_multiplier[i] > original_multiplier[i])
-				{
-				    color_multiplier[i] = original_multiplier[i];
-
-				    multipliers_modified = true;
-				}
-			}
-
-		    if (multipliers_modified)
-			colorcontrol_update(true);
-		} 
-	    else if (data == 0) 
-		{
-		    pr_info("%s: COLORCONTROL safety disabled\n", __FUNCTION__);
-
-		    safety_enabled = false;
-		} 
-	    else 
-		{
-		    pr_info("%s: invalid input range %u\n", __FUNCTION__, data);
-		}
+	    colorcontrol_update(v1_offsets);
 	} 
     else 
 	{
@@ -157,11 +38,6 @@ static ssize_t colorcontrol_safety_write(struct device * dev, struct device_attr
 	}
 
     return size;
-}
-
-static ssize_t colorcontrol_originalmultiplier_read(struct device * dev, struct device_attribute * attr, char * buf)
-{
-    return sprintf(buf, "%u %u %u\n", original_multiplier[0], original_multiplier[1], original_multiplier[2]);
 }
 
 static ssize_t colorcontrol_version(struct device * dev, struct device_attribute * attr, char * buf)
@@ -169,18 +45,12 @@ static ssize_t colorcontrol_version(struct device * dev, struct device_attribute
     return sprintf(buf, "%u\n", COLORCONTROL_VERSION);
 }
 
-static DEVICE_ATTR(v1_offset, S_IRUGO | S_IWUGO, colorcontrol_offset_read, colorcontrol_offset_write);
-static DEVICE_ATTR(multiplier, S_IRUGO | S_IWUGO, colorcontrol_multiplier_read, colorcontrol_multiplier_write);
-static DEVICE_ATTR(safety_enabled, S_IRUGO | S_IWUGO, colorcontrol_safety_read, colorcontrol_safety_write);
-static DEVICE_ATTR(original_multiplier, S_IRUGO, colorcontrol_originalmultiplier_read, NULL);
+static DEVICE_ATTR(v1_offsets, S_IRUGO | S_IWUGO, colorcontrol_v1offsets_read, colorcontrol_v1offsets_write);
 static DEVICE_ATTR(version, S_IRUGO , colorcontrol_version, NULL);
 
 static struct attribute *colorcontrol_attributes[] = 
     {
-	&dev_attr_v1_offset.attr,
-	&dev_attr_multiplier.attr,
-	&dev_attr_safety_enabled.attr,
-	&dev_attr_original_multiplier.attr,
+	&dev_attr_v1_offsets.attr,
 	&dev_attr_version.attr,
 	NULL
     };
