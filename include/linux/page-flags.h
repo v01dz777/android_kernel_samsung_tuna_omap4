ecc4bfbf606cb7e99a79429eefe8fb18826c7cc9
bsmitty83 <b671983@gmail.com>
2015-02-03 18:08:17 -0500

    follow-up to revert
    Conflicts:
    	include/linux/page-flags.h

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -224,15 +224,11 @@ PAGEFLAG(SavePinned, savepinned);			/* Xen */
 PAGEFLAG(Reserved, reserved) __CLEARPAGEFLAG(Reserved, reserved)
 PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
-__PAGEFLAG(SlobFree, slob_free)
 #ifdef CONFIG_KSM_CHECK_PAGE
 CLEARPAGEFLAG(KsmScan0, ksm_scan0) TESTSETFLAG(KsmScan0, ksm_scan0)
 CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
 #endif
 
-#ifdef CONFIG_CLEANCACHE
-PAGEFLAG(WasActive, was_active)
-#endif
 /*
  * Private page markings that may be used by the filesystem that owns the page
  * for its own purposes.
